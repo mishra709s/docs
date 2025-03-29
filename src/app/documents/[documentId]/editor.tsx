06750dfca8e7763c52c90e66f037fbf5d49f9bf2
mishra709s <mishra.709s@gmail.com>
2025-03-29 16:31:01 +0530

    added editor store using zustand

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -11,8 +11,35 @@ import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import { useEditor, EditorContent } from '@tiptap/react'
 
+import { useEditorStore } from '@/store/use=editor-source'
+
 export const Editor = () => {
+  const { setEditor } = useEditorStore()
   const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor)
+    },
+    onDestroy() {
+      setEditor(null)
+    },
+    onUpdate({ editor }) {
+      setEditor(editor)
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor)
+    },
+    onTransaction({ editor }) {
+      setEditor(editor)
+    },
+    onFocus({ editor }) {
+      setEditor(editor)
+    },
+    onBlur({ editor }) {
+      setEditor(editor)
+    },
+    onContentError({ editor }) {
+      setEditor(editor)
+    },
     editorProps: {
       attributes: {
         style: 'padding-left:56px; padding-right:56px',
